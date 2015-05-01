1c38cca830d30425f5b4fbfe92dd602dca0d1691
James <james.wilson@thirdlight.com>
2015-05-01 18:19:24 +0100

    attempt to clean up arg extraction, half way through the process

--- src/App/Args.hs
+++ src/App/Args.hs
@@ -1,7 +1,14 @@
-module App.Args (parse, parseKeys, ignoreKeys) where
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables, PartialTypeSignatures, TemplateHaskell #-}
 
-import qualified Data.List as L
-import qualified Data.Map  as M
+module App.Args (parse, parseKeys, ignoreKeys, runParamExtractor, getMaybe, getMaybeDef, setParam, E.throwError) where
+
+import qualified Data.List                 as L
+import qualified Data.Map                  as M
+import           Data.Default
+import           Control.Lens
+import           Control.Monad.Except      as E
+import           Control.Monad.Trans.State as S
+
 
 --very simple, just split args into keyed and "other", maintaining order of other.
 --keyed args begin with - or --. no protection against supplying args we dont want.
@@ -14,3 +21,23 @@ parseArgs (('-':'-':key):val:rest) keyed other = parseArgs rest (M.insert key va
 parseArgs (('-':key):val:rest) keyed other = parseArgs rest (M.insert key val keyed) other
 parseArgs (val:rest) keyed other = parseArgs rest keyed (other ++ [val])
 
+--
+-- a quick error+state monad to help extract args into a structure of your choosing,
+-- returning Left String if error or Right structure if success
+--
+type ParamExtractor p = StateT (M.Map String String,p) (Except String)
+
+runParamExtractor :: Default p => M.Map String String -> ParamExtractor p a -> Either String p
+runParamExtractor args m = fmap snd $ runExcept $ execStateT m (args,def)
+
+--handle arg extracting
+getMaybe err []     = E.throwError err
+getMaybe err (s:ss) = do
+    args <- fmap fst S.get
+    case M.lookup s args of
+        Nothing -> getMaybe err ss
+        Just a  -> return a
+
+setParam l a = assign (_2.l) a
+
+getMaybeDef err ss d = getMaybe err ss `E.catchError` (\_ -> return d)
--- src/HipChatClient.hs
+++ src/HipChatClient.hs
@@ -1,13 +1,12 @@
-{-# LANGUAGE OverloadedStrings, ScopedTypeVariables, PartialTypeSignatures #-}
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables, PartialTypeSignatures, TemplateHaskell #-}
 
 import qualified Network.WebSockets   as WS
 import           Network.Socket       (withSocketsDo) --only really necessary for windows
 import           Data.Text            (Text)
 import qualified Data.Text            as T
 import qualified Data.Text.IO         as T
-import           Data.Default         (def)
+import           Data.Default         (Default, def)
 import           Control.Concurrent 
-import qualified Control.Exception    as E
 import           Control.Applicative  ((<|>),(<$>),(<*>))
 import           Control.Monad.Trans  (liftIO)
 import           Control.Monad        (forever, forM_)
@@ -20,23 +19,39 @@ import qualified System.IO            as IO
 import           Network.Wreq
 import           Control.Lens         hiding ((.=))
 import           Data.Aeson.Lens
+import qualified Web.Scotty           as W;
 
 import           App.Args
 import           App.Messages
 
 chatloop hipchat authParam roomIds otherArgs = do
 
-    --parse port number from args
-    let (Just port) = (maybeP >>= readMaybe :: Maybe Int) <|> Just 9090
+    --parse port number of the websocket server from args
+    let (Just serverPort) = (maybeP >>= readMaybe :: Maybe Int) <|> Just 9090
           where maybeP = M.lookup "port" otherArgs <|> M.lookup "p" otherArgs
 
-    --parse address from args
-    let (Just address) =
-          M.lookup "address" otherArgs <|> M.lookup "a" otherArgs <|> Just "0.0.0.0"
+    --parse address of the websocket server from args
+    let (Just serverAddress) =
+          M.lookup "address" otherArgs <|> M.lookup "a" otherArgs <|> Just "127.0.0.1"
 
     --parse incoming port number (for hipchat webhooks) from args
-    let (Just incomingPort) = (maybeP >>= readMaybe :: Maybe Int) <|> Just 8080
-          where maybeP = M.lookup "incoming-port" otherArgs <|> M.lookup "ip" otherArgs
+    let (Just incomingPort) =  M.lookup "incoming-port" otherArgs <|> M.lookup "ip" otherArgs <|> Just "8080"
+    let mIncomingAddress =  M.lookup "incoming-address" otherArgs <|> M.lookup "ia" otherArgs <|> Nothing
+
+
+    let localAddress = "http://PUTADDRESSHERE:"++incomingPort
+
+    --kick off a web server to listen for messages
+
+    --register webhook to receive messages for each room we are in
+    forM_ roomIds $ \id -> do
+        postWith authParam (hipchat++"/v2/room/"++(show id)++"/webhook") $
+            toJSON $ object [
+                "url" .= (T.pack localAddress `mappend` "/message" :: T.Text),
+                "event" .= ("room_message" :: T.Text),
+                "name" .= ("messagehook" :: T.Text)
+            ]
+
 
     --send test message to desired room(s)
     forM_ roomIds $ \id -> do
@@ -72,21 +87,68 @@ app hipchat token otherArgs = do
         _  -> chatloop hipchat authParam roomIds otherArgs
 
 
+
+
+-- Handle our params with a little helper monad, below:
+--
+-- lensy param struct
+--
+data Params = Params {
+    _hipchatToken :: T.Text,
+    _hipchatAddress :: T.Text,
+    _serverAddress :: T.Text,
+    _serverPort :: Int,
+    _thisUrl :: T.Text,
+    _roomName :: T.Text
+} deriving (Show)
+
+instance Default Params where
+    def = Params {
+        _hipchatToken = "",
+        _hipchatAddress = "",
+        _serverAddress = "",
+        _serverPort = 9090,
+        _thisUrl = "",
+        _roomName = ""
+    }
+
+makeLenses ''Params
+
+
+
 main = do
     IO.hSetBuffering IO.stdout IO.NoBuffering
     putStrLn "Starting Client"
 
     argMap <- fmap parseKeys getArgs
 
-    --parse hipchat address from args
-    let mHipchatAddress = M.lookup "hipchat" argMap <|> M.lookup "h" argMap
+    --parse args here, returning error if something not properly set
+    let eParams = runParamExtractor argMap $ do
 
-    --auth token for hipchat API
-    let mToken = M.lookup "token" argMap <|> M.lookup "t" argMap
+         ha <- getMaybe "must provide a hipchat server address using --hipchat or -h" ["hipchat", "h"]
+         ht <- getMaybe "must provide a hipchat auth token with --token or -t" ["token", "t"]
 
-    case mToken of
-        Just token -> case mHipchatAddress of
-            Just hipchatAddress -> app hipchatAddress token argMap
-            Nothing -> putStrLn "must provide a hipchat server address using --hipchat or -h"
-        Nothing -> putStrLn "must provide a hipchat auth token with --token or -t"
+         setParam hipchatAddress (T.pack ha)
+         setParam hipchatToken (T.pack ht)
 
+         sa <- getMaybeDef "must provide websocket server address with --server-address or -sa" ["server-address", "sa"] "127.0.0.1"
+         sp <- getMaybeDef "must provide websocket server address with --server-address or -sa" ["server-port", "sp"] "9090"
+
+         let serverPortInt = readMaybe sp
+         case serverPortInt of
+            Nothing -> throwError "server port not a valid int"
+            Just p -> setParam serverPort p
+
+         setParam serverAddress (T.pack sa)
+         
+         ta <- getMaybe "must provide a hipchat auth token with --this-url or -tu" ["this-url", "tu"]
+
+         setParam thisUrl (T.pack ta)
+
+    --print error if we end up with one after extracting params, else
+    --pass params to app
+    case eParams of
+        Left err -> putStrLn err
+        Right p -> undefined
+
+    return ()
